a52b34448262b64753cb589a63e6cadaa6e8b086
Larry Archer <larcher@spoon.cx>
2014-03-10 21:41:27 -0500

    adding more explanation on how shells and awks work; don't hardcode the input filename :P

--- find-less-than.sh
+++ find-less-than.sh
@@ -4,9 +4,16 @@
 #   use cat -n to add a line number to each line
 #   use tr to strip <CR> and just leave tne \n newlines (in case this file was created on non-Unix, and we forgot to use eol="\n" in the write.table() call
 #   use awk to print only the lines where second field is greater than 0 but less than X
-threshold=$1
+#
+# usage: find-less-than.sh <threshold> <file>
+#    ex: find-less-than.sh 0.5 someoutputfile.out
 
-cat -n logpvalsOffspring.tsv | tr -d '\r' |  awk '( $2 > 0.0 ) && ( $2 < '$threshold' ) { print }'
+threshold=$1
+file=$2
+
+# Note: the $2 below is part of the awk program, and distinct from the shell
+# parameter variable $2 (since it's within single quotes ' ')
+cat -n "$file" | tr -d '\r' |  awk '( $2 > 0.0 ) && ( $2 < '$threshold' ) { print }'
 
 # Alternate form, find values greater than a threshold
-#cat -n logpvalsOffspring.tsv | awk '$2 > '$threshold' { print }'
+#cat -n logpvalsOffspring.tsv | tr -d '\r' | awk '$2 > '$threshold' { print }'
